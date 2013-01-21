1d2a016cd9aa5f666b3c311eb7a0cdb5613b9fc2
Carlos Martín Nieto <cmn@dwim.me>
2013-01-21 10:45:14 +0100

    Add repository:bare() and :references()

--- src/repository.erl
+++ src/repository.erl
@@ -1,8 +1,14 @@
 -module(repository, [Handle]).
--export([path/0, workdir/0]).
+-export([path/0, workdir/0, bare/0, references/0]).
 
 path() ->
     geef:repository_get_path(Handle).
 
 workdir() ->
     geef:repository_get_workdir(Handle).
+
+bare() ->
+    geef:repository_is_bare(Handle).
+
+references() ->
+    geef:reference_list(Handle).
