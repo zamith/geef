b671c5110e33752b51d0aa54f55c35be07695ad1
Carlos Martín Nieto <cmn@dwim.me>
2013-06-12 00:05:04 +0200

    Implement Access for Geef.Tree
    Make Geef.Tree its own record so we can define the protocol
    implementation for it. There shouldn't be any generic functions you'd
    want to call on a tree record.

--- lib/geef/tree.ex
+++ lib/geef/tree.ex
@@ -1,6 +1,6 @@
 defrecord Geef.TreeEntry, Record.extract(:geef_tree_entry, from: "src/geef_records.hrl")
 
-defmodule Geef.Tree do
+defrecord Geef.Tree, Record.extract(:geef_object, from: "src/geef_records.hrl") do
   alias Geef.Object
   alias Geef.TreeEntry
 
@@ -9,18 +9,29 @@ defmodule Geef.Tree do
   def lookup(repo, id) do
     case :geef_tree.lookup(repo, id) do
       {:ok, obj} ->
-        {:ok, Object.new obj}
+        {:ok, new obj}
       error ->
         error
     end
   end
 
-  def get(tree = Object[type: :tree], path) do
+  def get(tree, path) do
     case :geef_tree.get(rebind(tree), path) do
       {:ok, entry} ->
         {:ok, TreeEntry.new entry}
       error ->
         error
+    end
+  end
+
+end
+
+defimpl Access, for: Geef.Tree do
+
+  def access(tree, key) do
+    case Geef.Tree.get(tree, key) do
+      {:ok, entry} -> entry
+      {:error, _} -> nil
     end
   end
 
