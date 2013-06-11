44dbf9a78b27fe6adee1bb6eb436690ddd6c5874
Carlos Martín Nieto <cmn@dwim.me>
2013-06-11 17:31:32 +0200

    Make mix compile the NIF as well

--- mix.exs
+++ mix.exs
@@ -1,9 +1,18 @@
+defmodule Mix.Tasks.Compile.Rebar do
+  @shortdoc "Runs `rebar compile` so we build the NIF"
+
+  def run(_) do
+    Mix.shell.info System.cmd("rebar compile")
+  end
+end
+
 defmodule Geef.Mixfile do
   use Mix.Project
 
   def project do
     [ app: :geef,
       version: "0.0.1",
+      compilers: [:rebar, :elixir, :app],
       deps: deps ]
   end
 
