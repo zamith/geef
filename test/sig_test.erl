bd44d3f6b00e4c0c81f97144b541f2f3c31ee6ef
Carlos Martín Nieto <cmn@dwim.me>
2013-06-12 02:19:21 +0200

    Oops: make sig tests pass

--- test/sig_test.erl
+++ test/sig_test.erl
@@ -4,11 +4,11 @@
 -include("src/geef_records.hrl").
 
 now_test() ->
-    Sig = geef_sig:new("Carlos", "a@b.c"),
+    {ok, Sig} = geef_sig:new("Carlos", "a@b.c"),
     ?assertMatch(#geef_signature{name = <<"Carlos">>, email = <<"a@b.c">>, time=_}, Sig).
 
 at_test() ->
     Time = {os:timestamp(), 120},
-    Actual = geef_sig:new("Carlos", "a@b.c", Time),
+    {ok, Actual} = geef_sig:new("Carlos", "a@b.c", Time),
     Expected = #geef_signature{name = <<"Carlos">>, email = <<"a@b.c">>, time = Time},
     ?assertEqual(Expected, Actual).
