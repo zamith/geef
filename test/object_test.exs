895041b2a7c07186cf73116e2ab2fe572ed4728c
Carlos Martín Nieto <cmn@dwim.me>
2013-09-02 20:10:38 +0200

    tests: properly setup and teardown

--- test/object_test.exs
+++ test/object_test.exs
@@ -3,17 +3,21 @@ Code.require_file "../test_helper.exs", __FILE__
 defmodule ObjectTest do
   use ExUnit.Case
   use Geef
+  import RepoHelpers
 
-  defp tmp_dir do
-    {a, b, c} = :erlang.now()
-    n = node()
-    dir = :io_lib.format("geef-~p~p~p~p.git", [n, a, b, c])
-    Path.join(System.tmp_dir!, dir)
+  setup do
+    {{:ok, repo}, path} = tmp_bare()
+    {:ok, [repo: repo, path: path]}
   end
 
-  test "object OO helpers" do
-    dir = tmp_dir
-    { :ok, repo } = Repository.init(dir, true)
+  teardown meta do
+    Repository.stop(meta[:repo])
+    File.rm_rf!(meta[:path])
+    :ok
+  end
+
+  test "object OO helpers", meta do
+    repo = meta[:repo]
     { :ok, odb } = Repository.odb(repo)
     #{ :ok, index } = :geef_index.new
 
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,11 @@
 ExUnit.start
+
+defmodule RepoHelpers do
+  def tmp_bare do
+    {a, b, c} = :erlang.now()
+    n = node()
+    dir = :io_lib.format("geef-~p~p~p~p.git", [n, a, b, c])
+    path = Path.join(System.tmp_dir!, dir)
+    {Geef.Repository.init(path, true), path}
+  end
+end
