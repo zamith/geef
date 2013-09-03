ee1d719ef4e0c869c81398da561cec294fbae521
Carlos Martín Nieto <cmn@dwim.me>
2013-09-03 20:42:28 +0200

    Remove some dead code

--- src/geef_repo.erl
+++ src/geef_repo.erl
@@ -16,7 +16,7 @@
 
 %% API
 -export([open/1, init/2, path/1, workdir/1, odb/1, is_bare/1, references/1, discover/1,
-	 lookup_object/2, lookup_reference/2, revwalk/1, stop/1,
+	 lookup_object/2, revwalk/1, stop/1,
 	 reference_dwim/2, handle/1, iterator/2]).
 
 -include("geef_records.hrl").
@@ -83,10 +83,6 @@ lookup_object(Pid, Oid) ->
     gen_server:call(Pid, {lookup_object, Oid}).
 
 %% @private
-lookup_reference(Pid, Name) ->
-    gen_server:call(Pid, {lookup_reference, Name}).
-
-%% @private
 -spec iterator(pid(), iolist() | undefined) -> {ok, geef_ref:iterator()} | {error, term()}.
 iterator(Pid, Regexp) ->
     gen_server:call(Pid, {iterator, Regexp}).
@@ -133,9 +129,6 @@ handle_call(refs, _From, State = #state{handle=Handle}) ->
     {reply, Reply, State};
 handle_call({lookup_object, Oid}, _From, State = #state{handle=Handle}) ->
     Reply = geef_nif:object_lookup(Handle, Oid),
-    {reply, Reply, State};
-handle_call({lookup_reference, Name}, _From, State = #state{handle=Handle}) ->
-    Reply = geef_nif:reference_lookup(Handle, Name),
     {reply, Reply, State};
 handle_call({iterator, Regexp}, _From, State = #state{handle=Handle}) ->
     Reply = geef_nif:reference_iterator(Handle, Regexp),
