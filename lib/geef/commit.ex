568f9b2daf4fc22aa372dda5f2cca2fe318d2d65
Carlos Martín Nieto <cmn@dwim.me>
2013-11-24 16:34:08 +0100

    Wrap commit creation in elixir

--- lib/geef/commit.ex
+++ lib/geef/commit.ex
@@ -1,6 +1,10 @@
 defmodule Geef.Commit do
   alias Geef.Object
+  alias Geef.Signature
+  alias Geef.Oid
   import Object, only: :macros
+
+  @type t :: Object[type: :commit]
 
   def lookup(repo, id) do
     case :geef_commit.lookup(repo, id) do
@@ -13,6 +17,11 @@ defmodule Geef.Commit do
 
   def tree_id(commit = Object[type: :commit]) do
     :geef_commit.tree_id(rebind(commit))
+  end
+
+  @type create(pid, Signature.t, Signature.t, iolist, Tree.t, Commit.t, [atom]) :: {:ok, Oid.t} | {:error, term}
+  def create(repo, author = Signature[], committer = Signature[], message, tree, parents, opts // []) do
+    :geef_commit.create(repo, author, committer, message, tree, parents, opts)
   end
 
 end
